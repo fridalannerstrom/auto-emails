daf3a014e19ebcb9956e4a7ac790b6926fac76b1
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2024-12-26 13:16:36 +0000

    feat: search through and find email in database

--- run.py
+++ run.py
@@ -6,18 +6,35 @@ import os
 from notion_client import Client
 from pprint import pprint
 
-# Sätt din Notion-token som en miljövariabel i koden
+# Notion API token
 os.environ["NOTION_TOKEN"] = "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"
 
-# Initiera klienten
+# Connect to client
 notion = Client(auth=os.environ["NOTION_TOKEN"])
 
-# Databasens ID
+# Set database ID
 database_id = "168284e4604f8013a728d0aa102775aa"
 
-try:
-    # Fråga databasen
+def find_email_in_database(email):
+    """Search through the database for email"""
     response = notion.databases.query(database_id=database_id)
-    pprint(response)
-except Exception as e:
-    print(f"Ett fel inträffade: {e}")
+    for page in response["results"]:
+        properties = page["properties"]
+        if properties["E-mail"]["title"][0]["text"]["content"] == email:
+            return True  # Returnera True om e-posten finns
+    return False  # Returnera False om e-posten inte finns
+
+def main():
+    # Ask user för an email
+    email = input("Enter the email you want to search: ")
+
+    # Sök efter e-posten i databasen
+    email_found = find_email_in_database(email)
+
+    if email_found:
+        print(f"The email '{email}' is already in the database.")
+    else:
+        print(f"The email '{email}' is not in the database. ")
+
+if __name__ == "__main__":
+    main()
