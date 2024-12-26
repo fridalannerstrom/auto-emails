34162b046b3dfd59daddc277113fc1ed7f182919
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2024-12-26 11:18:59 +0000

    feat: integrate with Notion API

--- run.py
+++ run.py
@@ -2,28 +2,16 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-import gspread
-from google.oauth2.service_account import Credentials
+import os
+from notion_client import Client
+from pprint import pprint
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+# Sätt din Notion-token som en miljövariabel i koden
+os.environ["NOTION_TOKEN"] = "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"
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('e-mails')
+# Initiera klienten
+notion = Client(auth=os.environ["NOTION_TOKEN"])
 
-def get_email():
-    """
-    Get the email from user that should either be updated or added to spreadsheet
-    """
-    print("please enter the email.")
-    print("Example: john.doe@company.com\n")
-
-    data_email = input("Enter your email here:")
-    print(f"The email you provided is {data_email}")
-
-get_email()
+# Gör en förfrågan för att lista användare
+list_users_response = notion.users.list()
+pprint(list_users_response)
